956af957d0eceb957d9cd8d76c8ce80373209d0c
EC2 Default User <ec2-user@ip-172-31-19-133.eu-north-1.compute.internal>
2025-05-23 07:04:08 +0000

    deploy setting

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,17 @@ plugin :tmp_restart
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+rails_root = Dir.pwd
+# 本番環境のみデーモン起動
+if Rails.env.production?
+  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+  stdout_redirect(
+    File.join(rails_root, 'log', 'puma.log'),
+    File.join(rails_root, 'log', 'puma-error.log'),
+    true
+  )
+  # デーモン
+  daemonize
+end
